3a3fe7782028deac94504b9125661312527cd0ef
co6tter <co6tter@gmail.com>
2025-02-21 22:36:22 +0900

    feat: initialize project

---
+++ README.md
@@ -0,0 +1,15 @@
+# vite-vue-v1
+
+## Tech Stack
+
+- Vite
+- Vue 3
+- TypeScript
+- TailwindCSS
+- ESLint
+- Husky
+- lint-staged
+
+## Note
+
+- sample
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+const decrement = () => {
+  if (count.value > 0) {
+    count.value--;
+  }
+};
+</script>
+
+<template>
+  <div class="p-8">
+    <div class="max-w-lg mx-auto shadow-sm p-8 rounded-2xl">
+      <h1 class="text-xl">{{ msg }}</h1>
+      <section class="inline-flex gap-x-2 mt-4">
+        <h2>count</h2>
+        <p>
+          {{ count }}
+        </p>
+      </section>
+      <div class="flex gap-x-2 mt-4">
+        <button
+          class="bg-blue-200 px-4 py-2 rounded min-w-24"
+          @click="increment"
+        >
+          UP
+        </button>
+        <button
+          class="bg-red-200 px-4 py-2 rounded min-w-24"
+          @click="decrement"
+        >
+          DOWN
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
