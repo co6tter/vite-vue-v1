81a09d2a696b9e85230765355ac58c669e65abab
co6tter <co6tter@gmail.com>
2025-02-22 23:34:15 +0900

    chore: add grid

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+interface Props {
+  msg: string | number;
+}
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="bg-fuchsia-50 size-50">
+    <span class="text-2xl text-stone-400">{{ msg }}</span>
+  </div>
+</template>
